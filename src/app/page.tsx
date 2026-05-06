04f2f4258c5fa8e719251f905706b092ef5ecf2a
Sahil <sahil@Sahils-MacBook-Air.local>
2026-05-06 11:13:53 +0530

    chore: remove Republic Day campaign modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import { SocialProofSection } from "@/sections/SocialProofSection";
 import { ResumindDemo } from "@/sections/ResumindDemo";
 import { CaseStudyHighlights } from "@/sections/CaseStudyHighlights";
 import { FinalCTASection } from "@/sections/FinalCTASection";
-import HomeCampaignModal from "@/components/modals/HomeCampaignModal";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col">
-      <HomeCampaignModal />
       <Hero />
       <SocialProofSection />
       <ResumindDemo />
@@ -17,4 +15,3 @@ export default function HomePage() {
     </main>
   );
 }
-
